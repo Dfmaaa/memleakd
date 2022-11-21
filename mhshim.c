78590577e74e6847702f3c99df0ee04eacdf699a
Sameer Achhab <sameerkw23@gmail.com>
2022-11-22 00:45:31 +0530

    Comment.

--- mhshim.c
+++ mhshim.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include "mhshim.h"
-static int memory_allocated=0;
+static int memory_allocated=0; //Can be removed in future commits.
 void *malloc(size_t size){
     void (*real_malloc)(size_t)=dlsym(RTLD_NEXT,"malloc");
 }
